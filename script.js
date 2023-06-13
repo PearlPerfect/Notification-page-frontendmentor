5998bb8140083c576e39c03bc7e355e17a6a6bcc
PearlPerfect <zenithyonex4life@gmail.com>
2023-06-13 08:10:43 +0100

    correction

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const makeAsRead = document.querySelector("#mark");
 makeAsRead.addEventListener("click", () => {
     const unRead = document.querySelectorAll(".allUnread");
     const btn = document.getElementById("btn");
-    btn.style.display = "none"
+    btn.innerHTML = 0
     unRead.forEach(item => {
         item.className = "allread"
     });
